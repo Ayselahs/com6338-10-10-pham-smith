8fa4db36f2bdf0b6fe8d791c842ec821564508d9
Quynh <qpham2594@gmail.com>
2023-07-24 13:02:43 -0400

    update on js

--- voting.js
+++ voting.js
@@ -1,10 +1,10 @@
-let usersVoted = 0;
+// Voting.js
 
+  
 document.addEventListener("DOMContentLoaded", function() {
   const urlParams = new URLSearchParams(window.location.search);
   const userNumber = urlParams.get('user');
   const votingPageContainer = document.querySelector('.votingPage');
-
 
   // Retrieve the number of users and the randomly generated movies from local storage
   const numberOfUsers = parseInt(localStorage.getItem('numberOfUsers'));
@@ -33,21 +33,31 @@ document.addEventListener("DOMContentLoaded", function() {
   const doneButton = document.createElement('button');
   doneButton.innerHTML = 'Done!';
   doneButton.addEventListener("click", () => {
-    alert(`User ${userNumber} voted successfully!`);
-    usersVoted++;
-    
-    if (usersVoted === numberOfUsers) {
-      
-      const getResultButton = document.createElement("button");
-      getResultButton.innerHTML = "Get Result";
-      getResultButton.addEventListener("click", () => {
-        window.location.href = "results.html"
-      })
-      votingPageContainer.appendChild(getResultButton)
+    const selectedMovie = document.querySelector(`input[name=user${userNumber}Vote]:checked`);
+    if (selectedMovie) {
+      alert(`User ${userNumber} voted successfully for "${selectedMovie.value}"!`);
+      // Update local storage to indicate that this user has voted
+      localStorage.setItem(`user${userNumber}Vote`, selectedMovie.value);
+
+      // Check if all users have voted and send the message if necessary
+      const usersVoted = Array.from({ length: numberOfUsers }, (_, i) => i + 1)
+        .filter(user => localStorage.getItem(`user${user}Vote`)).length;
+        
+      if (usersVoted === numberOfUsers) {
+        // Send message to homepage to indicate all users have voted
+        window.parent.postMessage({ allUsersVoted: true }, '*');
+        localStorage.setItem('allUsersVoted', 'true');
+        console.log('everyone voted from voting')
+      }
+
+      // Close the voting tab after voting
       window.close();
+    } else {
+      alert("Please select a movie before clicking Done.");
     }
   });
 
   votingFormPage.appendChild(doneButton);
   votingPageContainer.appendChild(votingFormPage);
 });
+
