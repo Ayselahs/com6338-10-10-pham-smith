e80f2e4e9711d02b87b95b91a630d8137ff6bf17
Quynh <qpham2594@gmail.com>
2023-07-28 10:26:20 -0400

    comments for index.js and favorites.js

--- favorites.js
+++ favorites.js
@@ -1,17 +1,23 @@
+// display favorite movies when page is loaded
+
 document.addEventListener("DOMContentLoaded", function() {
     displayFavoriteMovies();
   });
   
+  // function to display the favorite movies
   function displayFavoriteMovies() {
     const favoriteMoviesListDiv = document.querySelector('.favoriteMoviesList');
     favoriteMoviesListDiv.innerHTML = ""; // Clear any previously displayed favorite movies
-  
+    
+    // using localStorage to retrieve the favorite movies
     const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
   
+    // if there is nothing saved, then the message of no favorite movies added yet will be on DOM
     if (favoriteMovies.length === 0) {
       const message = document.createElement('p');
       message.textContent = "No favorite movies added yet.";
       favoriteMoviesListDiv.appendChild(message);
+      // if there is any movie saved to favorites, then the display will be the same as the hompage for consistency
     } else {
       favoriteMovies.forEach((movie) => {
         const { id, poster_path, title, release_date, overview } = movie;
@@ -39,28 +45,26 @@ document.addEventListener("DOMContentLoaded", function() {
         movieLinks.classList.add('col-3', 'themed-grid-col');
         movieLinks.appendChild(link);
         movieDiv.appendChild(movieLinks);
-  
-        
-  
-        
-  
+        // View details will take place in another page (3rd page) to retrieve data from the 2nd API
+      
         // Create the Remove button
         const removeButton = document.createElement('button');
         removeButton.classList.add('col-3', 'themed-grid-col', 'shadow-sm');
         removeButton.setAttribute("id","removeBtn");
         removeButton.textContent = 'Remove';
         removeButton.addEventListener('click', () => {
-          removeFavoriteMovie(id);
+          removeFavoriteMovie(id);  // removing the favorite movie based on the individual id just as saving it
           // After removing the movie, update the displayed list of favorite movies
           displayFavoriteMovies();
         });
-  
+        
         movieDiv.appendChild(removeButton);
-        //favoriteMoviesListDiv.appendChild(movieCard);
       });
     }
   }
   
+  // when removing the movie from favorite list, it will filter the movie list and find the id that needs to be deleted to remove 
+  // localStorage will then update the new Favorite List
   function removeFavoriteMovie(movieId) {
     const favorites = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
     const updatedFavorites = favorites.filter((movie) => movie.id !== movieId);
